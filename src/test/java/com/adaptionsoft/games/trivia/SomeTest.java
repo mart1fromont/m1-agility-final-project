1c86e0283b3b3fd51d5029c4765efeffb3d4c3c0
Martin Fromont <62103174+mart1fromont@users.noreply.github.com>
2023-02-10 13:14:24 +0100

    Added tests

--- src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -1,14 +1,75 @@
 package com.adaptionsoft.games.trivia;
 
 
+import com.adaptionsoft.games.uglytrivia.Game;
 import org.junit.jupiter.api.Test;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SomeTest {
 
+    private final int GOLDEN_TESTS = 500;
+
     @Test
-    public void true_is_true() throws Exception {
-        assertTrue(false);
+    public void caracterizationTest() throws IOException {
+        for (int seed = 1; seed < GOLDEN_TESTS; seed++) {
+            testSeed(seed, false);
+        }
+    }
+
+    private void testSeed(int seed, boolean printExpected) throws IOException {
+        String expectedOutput = extractOutput(new Random(seed), new Game());
+        if (printExpected) {
+            System.out.println(expectedOutput);
+        }
+
+        final String filepath = "src\\test\\java\\com\\adaptionsoft\\games\\trivia\\results\\" + seed + ".txt";
+        final File file = new File(filepath);
+
+        if (!file.exists()) {
+            final FileWriter writer = new FileWriter(filepath);
+            writer.write(expectedOutput);
+            writer.close();
+            assert (true);
+        } else {
+            // Check if the content from the original code is still the same
+            final FileReader reader = new FileReader(filepath);
+            final BufferedReader bufferedReader = new BufferedReader(reader);
+            assertEquals(Files.readString(file.toPath()), expectedOutput);
+            bufferedReader.close();
+        }
+    }
+
+    private String extractOutput(Random rand, Game aGame) {
+        aGame.add("Chet");
+        aGame.add("Pat");
+        aGame.add("Sue");
+
+        PrintStream old = System.out;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try (PrintStream inmemory = new PrintStream(baos)) {
+            // WARNING: System.out.println() doesn't work in this try {} as the sysout is captured and recorded in memory.
+            System.setOut(inmemory);
+            boolean notAWinner;
+            do {
+                aGame.roll(rand.nextInt(5) + 1);
+
+                if (rand.nextInt(9) == 7) {
+                    notAWinner = aGame.wrongAnswer();
+                } else {
+                    notAWinner = aGame.wasCorrectlyAnswered();
+                }
+
+            } while (notAWinner);
+        } finally {
+            System.setOut(old);
+        }
+
+        return baos.toString();
     }
 }
